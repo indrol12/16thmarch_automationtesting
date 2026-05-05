3473b22a30740dd608e0ebf617cbf9a91bc3cf21
indrol12 <indrolofficial@gmail.com>
2026-05-05 09:46:03 +0530

    one more css selector has been added

--- src/main/java/test/Selenium_automation/cssselectorsyntax.java
+++ src/main/java/test/Selenium_automation/cssselectorsyntax.java
@@ -9,5 +9,6 @@ public static void main(String[] args) {
 	ChromeDriver driver =new ChromeDriver();
 	driver.get("https://demoqa.com/automation-practice-form");
 	driver.findElement(By.cssSelector("input#firstName")).sendKeys("poonam");
+   driver.findElement(By.cssSelector("input.mr-sm-2 form-control")).sendKeys("jadhav");
 }
 }
